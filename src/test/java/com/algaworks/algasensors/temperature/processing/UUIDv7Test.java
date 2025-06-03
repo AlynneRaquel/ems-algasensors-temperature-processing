795870612d40d8d44b1ac602fda262dce28a40ee
alynneraquel <alynneraquel@hotmail.com>
2025-06-03 19:49:59 -0300

    Usando o UUID v4 e v7 com Java

--- src/test/java/com/algaworks/algasensors/temperature/processing/UUIDv7Test.java
+++ src/test/java/com/algaworks/algasensors/temperature/processing/UUIDv7Test.java
@@ -1,23 +1,21 @@
 package com.algaworks.algasensors.temperature.processing;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 public class UUIDv7Test {
 
     @Test
     void sholdGenerateUUidv7() {
-        UUID uuid1 = IdGenerator.generateTimeBasedUUID();
-        UUID uuid2 = IdGenerator.generateTimeBasedUUID();
-        UUID uuid3 = IdGenerator.generateTimeBasedUUID();
-        UUID uuid4 = IdGenerator.generateTimeBasedUUID();
+        UUID uuid = IdGenerator.generateTimeBasedUUID();
 
-        System.out.println(uuid1);
-        System.out.println(uuid2);
-        System.out.println(uuid3);
-        System.out.println(uuid4);
+        OffsetDateTime uuidDateTime = UUIDv7Utils.extractOffsetDateTime(uuid).truncatedTo(ChronoUnit.MINUTES);
+        OffsetDateTime currentDateTime = OffsetDateTime.now().truncatedTo(ChronoUnit.MINUTES);
 
-        System.out.println(UUIDv7Utils.extractOffsetDateTime(uuid1));
+        Assertions.assertThat(uuidDateTime).isEqualTo(currentDateTime);
     }
 }
